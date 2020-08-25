f3336cbd24bf2861544912c889ca7c25e6efd8b6
Mumtaz <mumtaz.maqsood@yahoo.com>
2020-08-25 15:14:36 +0200

    adding dynamic methods in base package selenium_driver.py and use methods in login_page

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -1,45 +1,49 @@
 import time
 from selenium.webdriver.common.by import By
+from base.selenium_driver import SeleniumDriver
 
-class LoginPage:
+
+class LoginPage(SeleniumDriver):
+
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
 
     #Locators
-    _login_link = "//a[@class='navbar-link fedora-navbar-link']"
+    # _login_link = "//a[@class='navbar-link fedora-navbar-link']"
+    _login_link = "Login"
     _email_id = "user_email"
     _password_id = "user_password"
     _login_btn = "commit"
 #----------------------------------------------------------------------------------------------
 # getting home page elements
 # ----------------------------------------------------------------------------------------------
-    def getLoginLink(self):
-        return self.driver.find_element(By.XPATH, self._login_link)
-    def getEmailField(self):
-        return self.driver.find_element(By.ID, self._email_id)
-    def getPasswordField(self):
-        return self.driver.find_element(By.ID, self._password_id)
-    def getLoginBtn(self):
-        return self.driver.find_element(By.NAME, self._login_btn)
+#     def getLoginLink(self):
+#         return self.driver.find_element(By.XPATH, self._login_link)
+#     def getEmailField(self):
+#         return self.driver.find_element(By.ID, self._email_id)
+#     def getPasswordField(self):
+#         return self.driver.find_element(By.ID, self._password_id)
+#     def getLoginBtn(self):
+#         return self.driver.find_element(By.NAME, self._login_btn)
 
-# ----------------------------------------------------------------------------------------------
 
-# ----------------------------------------------------------------------------------------------
-    # enter and clicking  on home page elements
+#Actions performed on the elements , elementClick(), SendKeys() are the dynamic methods created in base class
 # ----------------------------------------------------------------------------------------------
     def clickLoginLink(self):
-        self.getLoginLink().click()
+        self.elementClick(self._login_link, locatorType="link")
     def enterEmail(self, email):
-        self.getEmailField().send_keys(email)
+        self.sendKeys(email, self._email_id)
     def enterPassword(self, password):
-        self.getPasswordField().send_keys(password)
-    def  clickLoginBtn(self):
-        self.getLoginBtn().click()
+        self.sendKeys(password, self._password_id)
+    def clickLoginBtn(self):
+        self.elementClick(self._login_btn, locatorType="name")
+# ----------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------
 
-
-
+# ----------------------------------------------------------------------------------------------
+#functionality, wrape all the actions
     def login(self, email, password):
         self.clickLoginLink()
         time.sleep(5)
