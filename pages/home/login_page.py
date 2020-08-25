d2d0bd163aada9373e0476e147a122d52da31af1
Mumtaz <mumtaz.maqsood@yahoo.com>
2020-08-25 13:33:21 +0200

    login page --> added locators & methods ---> become dynamic

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -5,13 +5,44 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def login(self, username, password):
-        login_link = self.driver.find_element(By.XPATH, "//a[@class='navbar-link fedora-navbar-link']")
-        login_link.click()
+    #Locators
+    _login_link = "//a[@class='navbar-link fedora-navbar-link']"
+    _email_id = "user_email"
+    _password_id = "user_password"
+    _login_btn = "commit"
+#----------------------------------------------------------------------------------------------
+# getting home page elements
+# ----------------------------------------------------------------------------------------------
+    def getLoginLink(self):
+        return self.driver.find_element(By.XPATH, self._login_link)
+    def getEmailField(self):
+        return self.driver.find_element(By.ID, self._email_id)
+    def getPasswordField(self):
+        return self.driver.find_element(By.ID, self._password_id)
+    def getLoginBtn(self):
+        return self.driver.find_element(By.NAME, self._login_btn)
+
+# ----------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------
+    # enter and clicking  on home page elements
+# ----------------------------------------------------------------------------------------------
+    def clickLoginLink(self):
+        self.getLoginLink().click()
+    def enterEmail(self, email):
+        self.getEmailField().send_keys(email)
+    def enterPassword(self, password):
+        self.getPasswordField().send_keys(password)
+    def  clickLoginBtn(self):
+        self.getLoginBtn().click()
+
+# ----------------------------------------------------------------------------------------------
+
+
+
+    def login(self, email, password):
+        self.clickLoginLink()
         time.sleep(5)
-        email_id = self.driver.find_element(By.ID, "user_email")
-        email_id.send_keys(username)
-        password_id = self.driver.find_element(By.ID, "user_password")
-        password_id.send_keys(password)
-        login_btn = self.driver.find_element(By.NAME, "commit")
-        login_btn.click()
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginBtn()
